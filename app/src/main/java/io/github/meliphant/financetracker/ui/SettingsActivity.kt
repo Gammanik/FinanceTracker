cb809f207a120eb009e390efb01247a79a39b8de
Meliphant <op.personal.analytics@gmail.com>
2018-07-28 19:00:40 +0300

    ListPrference for settings with enum data

--- app/src/main/java/io/github/meliphant/financetracker/ui/SettingsActivity.kt
+++ app/src/main/java/io/github/meliphant/financetracker/ui/SettingsActivity.kt
@@ -1,14 +1,14 @@
 package io.github.meliphant.financetracker.ui
 
 import android.os.Bundle
-import android.preference.PreferenceFragment
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.MenuItem
 import io.github.meliphant.financetracker.CurrencyRepository
-import io.github.meliphant.financetracker.network.CurrencyRespondResult
 import io.github.meliphant.financetracker.R
+import io.github.meliphant.financetracker.SettingsFragment
 import io.github.meliphant.financetracker.data.DataCurrencyRates
+import io.github.meliphant.financetracker.network.CurrencyRespondResult
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity(), CurrencyRespondResult {
@@ -19,26 +19,7 @@ class SettingsActivity : AppCompatActivity(), CurrencyRespondResult {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         CurrencyRepository().onCurrencyLoad(this)
-//        var settingsFragment = SettingsFragment()
-//        fragmentManager.beginTransaction()
-//                .replace(android.R.id.content, settingsFragment)
-//                .commit()
-
-        if (fragmentManager.findFragmentById(android.R.id.content) == null) {
-            fragmentManager.beginTransaction()
-                    .add(android.R.id.content, SettingsFragment())
-                    .commit()
-        }
-    }
-
-    override fun onCurrencySuccessLoad(currencyRates: DataCurrencyRates) {
-        Log.d("DataExchangeRates", "${currencyRates.rates}")
-//        Toast.makeText(this, "DataExchangeRates" + "${currencyRates.rates}", Toast.LENGTH_SHORT).show()
-
-    }
-
-    override fun onCurrencyErrorLoad() {
-        settings.setText(R.string.msg_currency_error)
+        addSettingsList()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -51,11 +32,21 @@ class SettingsActivity : AppCompatActivity(), CurrencyRespondResult {
         }
     }
 
-    class SettingsFragment : PreferenceFragment() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            addPreferencesFromResource(R.xml.settings_preferences)
+    private fun addSettingsList() {
+        if (fragmentManager.findFragmentById(android.R.id.content) == null) {
+            fragmentManager.beginTransaction()
+                    .add(android.R.id.content, SettingsFragment())
+                    .commit()
         }
     }
+
+    override fun onCurrencySuccessLoad(currencyRates: DataCurrencyRates) {
+        Log.d("DataExchangeRates", "${currencyRates.rates}")
+    }
+
+    override fun onCurrencyErrorLoad() {
+        settings.setText(R.string.msg_currency_error)
+    }
 }
+
 
