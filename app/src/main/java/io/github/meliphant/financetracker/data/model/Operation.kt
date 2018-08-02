403d65a4f4ae81e57e112ffefa0576090b4979e6
Bobyshev Nikita <Gammanik@gmail.com>
2018-08-02 19:29:21 +0300

    3: clean code

--- app/src/main/java/io/github/meliphant/financetracker/data/model/Operation.kt
+++ app/src/main/java/io/github/meliphant/financetracker/data/model/Operation.kt
@@ -2,13 +2,24 @@ package io.github.meliphant.financetracker.data.model
 
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.ForeignKey.CASCADE
 import android.arch.persistence.room.PrimaryKey
+import io.github.meliphant.financetracker.data.model.utils.MyCategory
 
-@Entity
+@Entity(
+//        foreignKeys = [
+//    (ForeignKey(
+//                entity = Wallet::class,
+//                parentColumns = ["id"],
+//                childColumns = ["walletId"],
+//                onDelete = CASCADE
+//        ))]
+)
 data class Operation(
         @PrimaryKey(autoGenerate = true) val id: Int? = null,
         val name: String,
         val amount: Double
-//        var category: MyCategory? = null,
-//        val wallet: Wallet? = null
+//        val category: MyCategory
+//        val wallet: Wallet
 )
--- app/src/main/java/io/github/meliphant/financetracker/data/model/other/DataOperation.kt
+++
@@ -1,7 +0,0 @@
-package io.github.meliphant.financetracker.data.model.other
-
-data class DataOperation(val amount: Double,
-                         val operationType: String,
-                         val currency: String,
-                         val category: String,
-                         val account: String)
